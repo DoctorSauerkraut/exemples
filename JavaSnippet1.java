fb62ec5749ff6d17561d7f4d9a7a1ede3825282b
DoctorSauerkraut <olivier.cros0@gmail.com>
2017-09-20 19:38:43 +0200

    Update JavaSnippet1.java

--- JavaSnippet1.java
+++ JavaSnippet1.java
@@ -1,7 +1,6 @@
 public class Voiture {
   private int puissance;
   private Couleur color;
-  private Distance kilometresParcourus;
   
   public void rouler() {
       Distance d = this.getDistanceTemporaire();
