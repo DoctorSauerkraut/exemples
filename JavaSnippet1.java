3fa83306b2604745e139753b5a0a794648b0098e
DoctorSauerkraut <olivier.cros0@gmail.com>
2017-09-20 19:29:36 +0200

    Create JavaSnippet1.java

---
+++ JavaSnippet1.java
@@ -0,0 +1,11 @@
+public class Voiture {
+  private int puissance;
+  private Couleur color;
+  
+  
+  public void rouler() {
+      Distance d = voiture.compterDistance();
+      d.incrementer(10);
+  }
+
+}
